b84c4347879f318ffb05ec048e018c8b359b32e7
abdeldjallil <abdeldjallil_ferchouche@hotmail.com>
2016-11-03 08:04:25 +0100

    javadoc et junit

---
+++ API/src/main/java/src/main/java/Test.java
@@ -0,0 +1,7 @@
+package src.main.java;
+
+public class Test {
+	public static void main(String[] args) {
+
+	}
+}
---
+++ API/src/test/java/src/main/java/TestTest.java
@@ -0,0 +1,14 @@
+package src.main.java;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestTest {
+
+	@Test
+	public void test() {
+		assertEquals(100, 100);
+	}
+
+}
